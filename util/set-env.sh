f76664db0909ac8fc7d8e9006d5c1dbeeac2a58e
mohamed <melsayed.se@gmail.com>
2022-05-25 16:54:18 +0200

    bug fixes

--- util/set-env.sh
+++ util/set-env.sh
@@ -1,5 +1,6 @@
+cat << EOF > ~/home/jenkins/env-file
 RDS_HOSTNAME=$(terraform -chdir=/terraform/ output --raw rds_hostname)
 RDS_PORT=$(terraform -chdir=/terraform/ output --raw rds_port)
-
 REDIS_HOSTNAME=$(terraform -chdir=/terraform/ output redis_hostname)
 REDIS_PORT=$(terraform -chdir=/terraform/ output redis_port)
+EOF
